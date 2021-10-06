f2b69e5a71c46076335b783c921a690185fbb03a
Scott Hutley <scotthutley1@comcast.net>
2021-10-06 13:39:47 -0700

    Add cuisine and restaurant class/constructors
    Co-authored-by: art-weinstein <art-weinstein@users.noreply.github.com>

---
+++ BestRestaurant/Models/Restaurant.cs
@@ -0,0 +1,13 @@
+namespace BestRestaurant.Models
+{
+  public class Restaurant
+  {
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
+    public string PhoneNumber { get; set; }
+    public int CuisineId { get; set; }
+    public virtual Cuisine Cuisine { get; set; }
+  }
+}
